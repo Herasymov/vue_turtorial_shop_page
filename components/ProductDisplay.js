15129a4c7f8e829735787319894b5fe6b8d9d571
Herasymov <Slavka.gs98@gmail.com>
2020-12-15 20:04:10 +0200

    task

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -37,10 +37,11 @@ app.component('product-display', {
           </div>
         </div>
       </div>
+      <review-list v-if='reviews.length' :reviews="reviews"></review-list>
+      <review-form @review-submitted="addReview"></review-form>
     </div>`,
     data() {
         return {
-
             product: 'Socks',
             brand: 'Vue Mastery',
             selectedVariant: 0,
@@ -48,7 +49,8 @@ app.component('product-display', {
             variants: [
                 { id: 2234, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 50 },
                 { id: 2235, color: 'green', image: './assets/images/socks_green.jpg', quantity: 0 },
-            ]
+            ],
+            reviews: []
         }
     },
     methods: {
@@ -57,6 +59,9 @@ app.component('product-display', {
         },
         updateVariant(index){
             this.selectedVariant = index
+        },
+        addReview(review) {
+            this.reviews.push(review)
         }
     },
     computed: {
