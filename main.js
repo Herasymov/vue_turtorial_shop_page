a980bb16cffecffcb5185a4337a76214da8ed68e
Herasymov <Slavka.gs98@gmail.com>
2020-12-15 16:31:18 +0200

    task

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ const app = Vue.createApp({
     data: function() {
         return {
             product: 'Socks',
-            image: './assets/images/Christmas-socks.jpg',
-            inventory: 0
+            image: './assets/images/socks_blue.jpg',
+            inStoke: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' },
+            ]
         }
     }
 })
